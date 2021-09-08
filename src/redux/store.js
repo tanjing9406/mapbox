9b19c83af8816c493cfef2b5ff5d3c57ef8f556b
tanjing9406 <1157185306@qq.com>
2021-09-08 18:10:55 +0800

    disabled serializableCheck

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers'
 
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false })
 })
 
 export default store
