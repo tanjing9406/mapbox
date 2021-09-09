66e6ac23d9de5b61f750878f3c7969ec3c45b41e
tanjing9406 <1157185306@qq.com>
2021-09-09 14:28:45 +0800

    update top menu page's ip address

--- src/config/top-menu.js
+++ src/config/top-menu.js
@@ -8,7 +8,7 @@ function BigData() {
     return <iframe
         width="100%"
         height="100%"
-        src="http://192.168.7.122/admin"
+        src="http://10.100.0.122/admin"
     ></iframe>
 }
 
@@ -16,7 +16,7 @@ function DataDashboard() {
     return <iframe
         width="100%"
         height="100%"
-        src="http://192.168.7.122/CSGbigScreen"
+        src="http://10.100.0.122/CSGbigScreen"
     ></iframe>
 }
 
