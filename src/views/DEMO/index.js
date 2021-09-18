de12ec166e0930e8deaede3b0eef2fcd79e2e77b
tanjing9406 <1157185306@qq.com>
2021-09-18 10:42:46 +0800

    add view template

---
+++ src/views/DEMO/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import './style.less'
+
+function Demo(){
+    return (
+        <div className="demoPage absolute bg-white mt30 ml30 py6 px18">敬请期待！</div>
+    )
+}
+
+export default Demo
