ab5dbcecae67ebc3db7e7e32bc24090d9f24ff3b
tanjing9406 <1157185306@qq.com>
2021-10-27 14:19:13 +0800

    update/baseproduction: changed post msg

--- src/views/iframes/baseproduction/index.js
+++ src/views/iframes/baseproduction/index.js
@@ -7,12 +7,7 @@ function BaseProduction() {
 
     const loaded = () => {
         const ifrm = document.getElementById('baseproduction-iframe').contentWindow;
-        ifrm.postMessage({
-            ACCESSTOKEN: '96f935bc-bf5c-4756-9ed7-07fb1b47583e',
-            REFRESHTOKEN: '83766113-a3b2-4b3f-8aec-36f1e80b1b3c',
-            EXPIRESIN: 83788,
-            TOKENTYPE: 'bearer'
-        }, iframe_url);
+        ifrm.postMessage('96f935bc-bf5c-4756-9ed7-07fb1b47583e', iframe_url);
     }
 
     return (
